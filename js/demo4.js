613fc07085408d0122935e483238a5e6c19b5573
Mark Everett <themynnx@gmail.com>
2017-09-28 20:51:14 -0700

    Refactoring Scene from Tunnel

--- js/demo4.js
+++ js/demo4.js
@@ -5,22 +5,26 @@ var wh = window.innerHeight;
 var isMobile = ww < 500;
 var WACKY_COLORS = [0x9effb8, 0x89aee1, 0xd46ce7, 0xe9f259, 0x7cf4d3];
 
-function Tunnel(objects, textures) {
+function Scene(objects, textures) {
 
   window.cell = objects.cell.object.children[0];
 
   this.init(objects);
-  this.createMesh(textures);
+  this.tunnel = new Tunnel(
+    this.scene,
+    {
+      texture: textures.microdots.texture,
+      bump: textures.microdotsBump.texture
+    }
+  );
 
   this.handleEvents();
 
   window.requestAnimationFrame(this.render.bind(this));
 }
 
-Tunnel.prototype.init = function(objects) {
+Scene.prototype.init = function(objects) {
 
-  this.speed = 1;
-  this.currentColor = 0;
   this.prevTime = 0;
 
   this.mouse = {
@@ -53,44 +57,24 @@ Tunnel.prototype.init = function(objects) {
   this.addBoss(objects.virus.object);
 };
 
-function Boss(object) {
-  var radius = Math.random() * 0.003 + 0.0003;
-  var geom = object.children[0].geometry;
-  var mat = new THREE.MeshPhongMaterial({
-    color: new THREE.Color(WACKY_COLORS[Math.floor(Math.random()*WACKY_COLORS.length)])
-    // shading: THREE.FlatShading
-  });
+Scene.prototype.handleEvents = function() {
+  window.addEventListener('resize', this.onResize.bind(this), false);
 
-  this.mesh = new THREE.Mesh(geom, mat);
-  var scale = radius / 14;
-  this.mesh.scale.set(scale, scale, scale);
-  this.mesh.position.set(0, 0, 0.55);
+  document.body.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+  document.body.addEventListener('touchmove', this.onMouseMove.bind(this), false);
 
-  this.offset = new THREE.Vector3(0, 0, 0);
-  this.speed = Math.random() * 0.004 + 0.0002;
-  this.rotate = new THREE.Vector3(-Math.random() * 0.1 + 0.01, 0, Math.random() * 0.01);
+  document.body.addEventListener('mousedown', this.setMouseDown.bind(this, true), false);
+  document.body.addEventListener('mouseup', this.setMouseDown.bind(this, false), false);
+};
 
-  this.pos = new THREE.Vector3(0, 0, 0);
-  return this;
-}
-// Boss.prototype = Particle.prototype;
-Boss.prototype.update = function(tunnel) {
-  this.percent += this.speed * (this.burst ? 1 : tunnel.speed);
-
-  this.pos = tunnel.curve.getPoint(1 - (tunnel.speed % 1)).add(this.offset);
-  this.mesh.position.x = this.pos.x;
-  this.mesh.position.y = this.pos.y;
-  this.mesh.position.z = this.pos.z;
-}
-
-Tunnel.prototype.addBoss = function(object) {
+Scene.prototype.addBoss = function(object) {
   this.boss = new Boss(object);
   this.offset = new THREE.Vector3((Math.random() - 0.5) * 0.025, (Math.random() - 0.5) * 0.025, 0);
   this.boss.percent = 0.1;
   this.scene.add(this.boss.mesh);
 };
 
-Tunnel.prototype.addParticle = function() {
+Scene.prototype.addParticle = function() {
   this.particles = [];
   this.particlesContainer = new THREE.Object3D();
   this.scene.add(this.particlesContainer);
@@ -101,7 +85,14 @@ Tunnel.prototype.addParticle = function() {
   }
 };
 
-Tunnel.prototype.createMesh = function(textures) {
+function Tunnel(scene, texture) {
+  this.scene = scene;
+  this.speed = 0.5;
+  this.currentColor = 0;
+  this.createMesh(texture);
+}
+
+Tunnel.prototype.createMesh = function(texture) {
   var points = [];
   var i = 0;
   var geometry = new THREE.Geometry();
@@ -120,8 +111,8 @@ Tunnel.prototype.createMesh = function(textures) {
 
   this.tubeMaterial = new THREE.MeshStandardMaterial({
     side: THREE.BackSide,
-    map: textures.microdots.texture,
-    bumpMap: textures.microdotsBump.texture,
+    map: texture.texture,
+    bumpMap: texture.bump,
     bumpScale: 0.0005
   });
 
@@ -139,21 +130,11 @@ Tunnel.prototype.createMesh = function(textures) {
   this.scene.add(tubeMesh);
 };
 
-Tunnel.prototype.handleEvents = function() {
-  window.addEventListener('resize', this.onResize.bind(this), false);
-
-  document.body.addEventListener('mousemove', this.onMouseMove.bind(this), false);
-  document.body.addEventListener('touchmove', this.onMouseMove.bind(this), false);
-
-  document.body.addEventListener('mousedown', this.setMouseDown.bind(this, true), false);
-  document.body.addEventListener('mouseup', this.setMouseDown.bind(this, false), false);
-};
-
-Tunnel.prototype.setMouseDown = function(setTo) {
+Scene.prototype.setMouseDown = function(setTo) {
   this.mouseDown = setTo;
 };
 
-Tunnel.prototype.onResize = function() {
+Scene.prototype.onResize = function() {
   ww = window.innerWidth;
   wh = window.innerHeight;
 
@@ -164,7 +145,7 @@ Tunnel.prototype.onResize = function() {
   this.renderer.setSize(ww, wh);
 };
 
-Tunnel.prototype.onMouseMove = function(e) {
+Scene.prototype.onMouseMove = function(e) {
   if (e.type === 'mousemove') {
     this.mouse.target.x = e.clientX;
     this.mouse.target.y = e.clientY;
@@ -172,9 +153,11 @@ Tunnel.prototype.onMouseMove = function(e) {
     this.mouse.target.x = e.touches[0].clientX;
     this.mouse.target.y = e.touches[0].clientY;
   }
+
+  this.tunnel.mouse.target = this.mouse.target;
 };
 
-Tunnel.prototype.updateCameraPosition = function() {
+Scene.prototype.updateCameraPosition = function() {
   this.mouse.position.x += (this.mouse.target.x - this.mouse.position.x) / 30;
   this.mouse.position.y += (this.mouse.target.y - this.mouse.position.y) / 30;
 
@@ -185,6 +168,8 @@ Tunnel.prototype.updateCameraPosition = function() {
   this.camera.rotation.y = Math.PI - (this.mouse.ratio.x * 0.3 - 0.15);
   this.camera.position.x = ((this.mouse.ratio.x) * 0.044 - 0.025);
   this.camera.position.y = ((this.mouse.ratio.y) * 0.044 - 0.025);
+
+  this.tunnel.mouse = this.mouse;
 };
 
 Tunnel.prototype.updateCurve = function() {
@@ -218,27 +203,30 @@ Tunnel.prototype.updateMaterialOffset = function() {
   this.tubeMaterial.map.offset.x += this.speed / 25;
 };
 
-Tunnel.prototype.updateJoystickValues = function() {
+Scene.prototype.updateJoystickValues = function() {
   var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
   if (!gamepads[0]) {
     return;
   }
 
-
   var gamepad = gamepads[0];
 
-  this.speed = (-1 * gamepad.axes[6] + 1);
-  this.mouseDown = gamepad.buttons[0].pressed;
+  this.tunnel.setSpeed(-1 * gamepad.axes[6] + 1);
+  if (gamepad.buttons[0].pressed) {
+    this.addBurstParticle();
+  }
   this.debugReload(gamepad.buttons[7].pressed);
-  this.updateColor(gamepad.buttons[11].pressed);
+  this.tunnel.updateColor(gamepad.buttons[11].pressed);
 
   // input = [-1, -0.5,   0,      0.5, 1]
   // output = [0, vw / 4, vw / 2, 3vw/4, 1vw];
   this.mouse.target.x = ((gamepads[0].axes[0] + 1) / 2) * ww;
   this.mouse.target.y = ((-1 * gamepads[0].axes[1] + 1) / 2) * wh;
+
+  this.tunnel.mouse = this.mouse;
 };
 
-Tunnel.prototype.debugReload = (function() {
+Scene.prototype.debugReload = (function() {
   var hasBeenFalse = false;
   return function(shouldReload) {
     hasBeenFalse = hasBeenFalse || shouldReload === false;
@@ -265,43 +253,39 @@ Tunnel.prototype.updateColor = _.throttle(function (shouldSwitch) {
   }
 }, 500);
 
-Tunnel.prototype.render = function(time) {
-  this.updateJoystickValues();
-  this.updateMaterialOffset();
-  this.updateCameraPosition();
-  this.updateCurve();
-  this.boss.update(this);
+Tunnel.prototype.setSpeed = function(speed) { this.speed = speed; };
 
+Tunnel.prototype.update = function() {
+  this.updateMaterialOffset();
+  this.updateCurve();
+};
+
+Scene.prototype.updateParticles = function() {
   for (var i = 0; i < this.particles.length; i++) {
-    this.particles[i].update(this);
+    this.particles[i].update(this.tunnel.speed, this.tunnel.curve);
     if (this.particles[i].burst && this.particles[i].percent > 1) {
       this.particlesContainer.remove(this.particles[i].mesh);
       this.particles.splice(i, 1);
       i--;
     }
   }
+};
 
-  // When mouse down, add a lot of shapes
-  if (this.triggerPressed || this.mouseDown) {
-    if (time - this.prevTime > 20) {
-      this.prevTime = time;
-      var particle = new Particle(this.scene, true);
-      this.particles.push(particle);
-      this.particlesContainer.add(particle.mesh);
-      if (!isMobile) {
-        particle = new Particle(this.scene, true);
-        this.particles.push(particle);
-        this.particlesContainer.add(particle.mesh);
+Scene.prototype.addBurstParticle = _.throttle(function () {
+  var particle = new Particle(this.scene, true);
+  this.particles.push(particle);
+  this.particlesContainer.add(particle.mesh);
+}, 20);
 
-        particle = new Particle(this.scene, true);
-        this.particles.push(particle);
-        this.particlesContainer.add(particle.mesh);
-      }
-    }
-  }
+Scene.prototype.render = function() {
+  this.updateJoystickValues();
+  this.updateCameraPosition();
+
+  this.tunnel.update();
+  this.updateParticles();
+  this.boss.update(this.tunnel);
 
   this.renderer.render(this.scene, this.camera);
-
   window.requestAnimationFrame(this.render.bind(this));
 };
 
@@ -345,11 +329,11 @@ function Particle(scene, burst) {
   this.pos = new THREE.Vector3(0, 0, 0);
 }
 
-Particle.prototype.update = function(tunnel) {
+Particle.prototype.update = function(tunnelSpeed, tunnelCurve) {
 
-  this.percent += this.speed * (this.burst ? 1 : tunnel.speed);
+  this.percent += this.speed * (this.burst ? 1 : tunnelSpeed);
 
-  this.pos = tunnel.curve.getPoint(1 - (this.percent % 1)).add(this.offset);
+  this.pos = tunnelCurve.getPoint(1 - (this.percent % 1)).add(this.offset);
   this.mesh.position.x = this.pos.x;
   this.mesh.position.y = this.pos.y;
   this.mesh.position.z = this.pos.z;
@@ -358,13 +342,45 @@ Particle.prototype.update = function(tunnel) {
   this.mesh.rotation.z += this.rotate.z;
 };
 
+function Boss(object) {
+  var radius = Math.random() * 0.003 + 0.0003;
+  var geom = object.children[0].geometry;
+  var mat = new THREE.MeshPhongMaterial({
+    color: new THREE.Color(WACKY_COLORS[Math.floor(Math.random()*WACKY_COLORS.length)])
+    // shading: THREE.FlatShading
+  });
+
+  this.mesh = new THREE.Mesh(geom, mat);
+  var scale = radius / 14;
+  this.mesh.scale.set(scale, scale, scale);
+  this.mesh.position.set(0, 0, 0.55);
+
+  this.offset = new THREE.Vector3(0, 0, 0);
+  this.speed = Math.random() * 0.004 + 0.0002;
+  this.rotate = new THREE.Vector3(-Math.random() * 0.1 + 0.01, 0, Math.random() * 0.01);
+
+  this.pos = new THREE.Vector3(0, 0, 0);
+  return this;
+}
+
+// Boss.prototype = Particle.prototype;
+Boss.prototype.update = function(tunnel) {
+  this.percent += this.speed * (this.burst ? 1 : tunnel.speed);
+
+  this.pos = tunnel.curve.getPoint(1 - (tunnel.speed % 1)).add(this.offset);
+  this.mesh.position.x = this.pos.x;
+  this.mesh.position.y = this.pos.y;
+  this.mesh.position.z = this.pos.z;
+}
+
+
 function init() {
   var textures = null;
   var objects = null;
 
   function checkDone() {
     if (textures && objects) {
-      window.tunnel = new Tunnel(objects, textures);
+      window.tunnel = new Scene(objects, textures);
     }
   }
   loadTextures(function(ts) { textures = ts; checkDone(); });
